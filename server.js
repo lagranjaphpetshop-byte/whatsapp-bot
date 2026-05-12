198b3d8a4aceb81c94e3bc89a2ed3a4e5e4c3250
petsh <lagranjaphpetshop@gmail.com>
2026-05-12 12:19:41 -0500

    limpieza secretos

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -374,8 +375,9 @@ Escribe otro número de horario`;
 // SERVIDOR
 // =====================================
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
     console.log(
         "Servidor corriendo en puerto 3000"
     );
